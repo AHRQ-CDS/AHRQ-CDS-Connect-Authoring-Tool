c4a75075e30b26fe317bf29c9460af43796326db
Julia Afeltra <jafeltra@mitre.org>
2017-03-28 14:36:58 -0400

    Save a text file with no meaningful content

--- src/components/BuilderPalette.js
+++ src/components/BuilderPalette.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 
 class BuilderPalette extends Component {
+  exportFile() {
+    let finalText = 'String to save!';
+
+    let saveElement = document.createElement('a');
+    saveElement.href = 'data:text/plain,' + encodeURIComponent(finalText);
+    saveElement.download = 'SaveMe.txt';
+    saveElement.click();
+  }
+
   render() {
     return (
       <nav className="builder-palette inline-nav">
@@ -10,6 +19,7 @@ class BuilderPalette extends Component {
           <li><FontAwesome className='fa-fw' name='stethoscope' /> Conditions</li>
           <li><FontAwesome className='fa-fw' name='bug' /> Symptoms</li>
           <li><FontAwesome className='fa-fw' name='medkit' /> Medications</li>
+          <li onClick={this.exportFile}><FontAwesome className='fa-fw' name='floppy-o' /> Save </li>
         </ul>
       </nav>
     );
