37f082e051b0e9b5d080a4405c22d80c6b66b3a3
Julia Afeltra <jafeltra@mitre.org>
2017-03-29 16:01:00 -0400

    Took out old comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import '../node_modules/font-awesome/css/font-awesome.css';
 import './styles/App.css';
-// import Item from './Item';
-// import DropBox from './DropBox';
-// import SaveButton from './SaveButton';
 
 class App extends Component {
-  /* <Item />
-        <DropBox />
-        <SaveButton />
-        */
   render() {
     return (
       <div>
