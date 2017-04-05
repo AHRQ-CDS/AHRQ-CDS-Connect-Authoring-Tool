0abdc20ef6792522263285fa0f19fadfa344605c
Cristen Jones <cbjones@mitre.org>
2017-04-05 15:10:12 -0400

    add some more accessibility testing with aXe

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-
+import axe from 'react-axe';
 import App from './App';
 import Navbar from './components/Navbar';
 import AuthorPage from './components/AuthorPage';
 import BuilderPage from './components/BuilderPage';
 
 const history = createBrowserHistory();
+
+// log accessibility errors to the console.
+// works in Chrome, with limited functionality in Safari and Firefox
+if (process.env.NODE_ENV === 'development') {
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
   <Router history={history}>
