c75a75ed804704967b51b9307d595e21f4f9be40
Julia Afeltra <jafeltra@mitre.org>
2017-04-24 11:50:57 -0400

    Small changes to save template instances and routes for saving in database

--- app/models/artifact.js
+++ app/models/artifact.js
@@ -6,7 +6,8 @@ let Schema = mongoose.Schema;
 let ArtifactSchema = new Schema({
   name: String,
   version: String,
-  template_instances: [{ type: Schema.Types.ObjectId, ref: 'TemplateInstance' }]
+  template_instances: Array // { type: mongoose.Schema.Types.ObjectId, ref: 'TemplateInstance' }
+  // In order to reference the TemplateInstance by ObjectId, it needs to be saved first
 },{
   timestamps: true // adds created_at, updated_at
 });
