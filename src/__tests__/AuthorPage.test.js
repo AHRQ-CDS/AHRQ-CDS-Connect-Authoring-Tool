87b22122a95aca5cdb4901adb297940c6f0d4066
Andre Quina <andre.quina@gmail.com>
2017-05-01 09:40:29 -0400

    authors is not something that is in scope currently. longer term a list of authors probably makes more sense as user accounts rather than as a list that can be added/edited

--- src/__tests__/AuthorPage.test.js
+++
@@ -1,7 +0,0 @@
-import AuthorPage from '../components/AuthorPage';
-import { shallowRenderComponent } from '../helpers/test_helpers';
-
-test('App renders without crashing', () => {
-  const component = shallowRenderComponent(AuthorPage);
-  expect(component).toBeDefined();
-});
--- src/components/AuthorPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AuthorBox from './AuthorBox';
-
-export default () => (
-  <AuthorBox
-    url='http://localhost:3001/api/authors'
-    pollInterval={2000}
-  />
-);
