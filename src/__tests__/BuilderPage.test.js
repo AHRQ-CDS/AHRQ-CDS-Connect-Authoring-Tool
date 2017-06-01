496bc4147f65370ceda4d5c67aa2d086d092c2c1
craigmcginley <craigmcginley@gmail.com>
2017-06-01 14:56:15 -0400

    fix yarn.lock and failing test   - rebuild yarn.lock file   - fix failing tests in BuilderPage.test.js

--- src/__tests__/BuilderPage.test.js
+++ src/__tests__/BuilderPage.test.js
@@ -23,13 +23,17 @@ test('match results in selected group', () => {
 });
 
 test('children have correct classes', () => {
-  const classNames = ['builder__header', 'builder__sidebar', 'builder__canvas'];
+  const classNames = ['builder__header', 'builder__panel', 'builder__canvas'];
   component.children().forEach((node, i) => {
     expect(node.hasClass(classNames[i])).toBeTruthy();
+
+    if (classNames[i] === 'builder__panel') {
+      expect(node.find('.builder__sidebar')).toHaveLength(1);
+    }
   });
 });
 
 test('renders with children with or without a route match', () => {
-  expect(component.children()).toHaveLength(3);
-  expect(componentWithMatch.children()).toHaveLength(3);
+  expect(component.children()).toHaveLength(2);
+  expect(componentWithMatch.children()).toHaveLength(2);
 });
