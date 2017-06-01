55b7852569336c5e9aac29209a91a86866c00410
Andre Quina <andre.quina@gmail.com>
2017-06-01 13:15:42 -0400

    remove validation on string parameter

--- src/components/builder/parameters/StringParameter.js
+++ src/components/builder/parameters/StringParameter.js
@@ -4,27 +4,19 @@ import _ from 'lodash';
 /**
  * props are from a templateInstance parameters object,
  * and a function called UpdateInstance that takes an object with
- * key-value pairs that represents that state of the templateInstance,
- * and a function called Validation that validates what is entered
+ * key-value pairs that represents that state of the templateInstance
  */
 class StringParameter extends Component {
   constructor(props) {
     super(props);
-    this.state = { valid: null }; // no validation at start
   }
 
   render() {
     const id = _.uniqueId('parameter-');
     const helpId = _.uniqueId('parameter-help-');
-    let validityClassName = null;
-    if (this.state.valid) {
-      validityClassName = 'form__group-valid';
-    } else if (this.state.valid === false) {
-      validityClassName = 'form__group-invalid';
-    }
 
     return (
-      <div className={`form__group ${validityClassName}`}>
+      <div className={'form__group'}>
         <label htmlFor={id}>
           {this.props.name}:
 
@@ -36,18 +28,10 @@ class StringParameter extends Component {
             onChange={(event) => {
               const name = event.target.name;
               const value = event.target.value;
-              this.setState({ valid: this.props.validation(value) });
-              if (this.state.valid) {
-                this.props.updateInstance({ [name]: value });
-              }
+              this.props.updateInstance({ [name]: value });
             }}
           />
         </label>
-        <span id={helpId} role='alert' aria-live='assertive'>
-        { this.state.valid === false
-          ? 'Spaces are prohibited in element names' /* TODO: Have validation message provided by the validation function or some other prop */
-          : null }
-        </span>
       </div>
     );
   }
