eb72625ecd6c74149c3d3103e06f55c237b582fa
David Connolly <dpc15038@gmail.com>
2017-07-10 08:36:44 -0400

    Accidentally commented accessibility errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import axe from 'react-axe';
+import axe from 'react-axe';
 import Routes from './routes';
 
 // log accessibility errors to the console.
 // works in Chrome, with limited functionality in Safari and Firefox
-// if (process.env.NODE_ENV === 'development') {
-//   axe(React, ReactDOM, 1000);
-// }
+if (process.env.NODE_ENV === 'development') {
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
   <Routes />,
