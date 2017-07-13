fd93207467b6492c2a6a27d3dc32e5bd9acf74c1
Agustin Mora <amora@mitre.org>
2017-07-13 08:18:42 -0400

    fixed backend models

--- app/models/artifact.js
+++ app/models/artifact.js
@@ -6,7 +6,8 @@ let Schema = mongoose.Schema;
 let ArtifactSchema = new Schema({
   name: String,
   version: String,
-  instanceTree: Object
+  expTreeInclude: Object,
+  expTreeExclude: Object
 },{
   timestamps: true // adds created_at, updated_at
 });
