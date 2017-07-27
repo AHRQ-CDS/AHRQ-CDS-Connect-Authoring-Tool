fa1ab52ec6a7d4b1c0e30bfe8736ddfa662ed1b0
David Connolly <dpc15038@gmail.com>
2017-07-27 15:16:14 -0400

    Corrected value for special subpops

--- src/components/builder/ErrorStatement.js
+++ src/components/builder/ErrorStatement.js
@@ -21,7 +21,13 @@ class ErrorStatement extends Component {
   options = () => {
     const dropdown = [{label: 'Recommendations is null', value:'"Recommendations" is null'}];
     const params = this.props.booleanParameters.map(p => {return({label: p.name, value: p.value})});
-    const subpops = this.props.subpopulations.map(s => {return({label: s.subpopulationName, value: s.subpopulationName})});
+    const subpops = this.props.subpopulations.map(s => {
+      if (s.special) {
+        return({label: s.subpopulationName, value: s.special_subpopulationName});
+      } else {
+        return({label: s.subpopulationName, value: s.subpopulationName});
+      }
+    });
     return dropdown.concat(params).concat(subpops);
   }
 
