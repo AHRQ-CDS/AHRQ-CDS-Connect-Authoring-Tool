5f04426831830708951995a4d4b6482d277a4851
Cristen Jones <cbjones@mitre.org>
2017-07-31 12:06:50 -0400

    Add text and links to the home page.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import 'react-select/dist/react-select.css';
 
 export default () => (
   <div>
-    <h1>Welcome to the CDS Authoring Tool</h1>
-    <p>Build your first Clinicial Decision Support artifact by clicking <NavLink to="/build">here</NavLink>.</p>
+    <h2>Welcome to the Clinical Decision Support Authoring Tool</h2>
+    <p>Build and export your first Clinicial Decision Support artifact by clicking <NavLink to="/build">here</NavLink>. </p>
+
+    <h3>About</h3>
+    <p>The <strong>CDS Authoring Tool</strong> is a key part of CDS Connect, a project sponsored by the <a href="https://www.ahrq.gov/">Agency for Healthcare Research and Quality</a> that seeks to generate a systematic and replicable process for transforming patient-centered outcomes research (PCOR) findings into shareable and standards-based clinical decision support (CDS) electronic artifacts. It is hoped that this publicly-available authoring tool will promote the creation and use of CDS in everyday clinical settings, and that it will serve as the linchpin for connecting high-quality CDS to the U.S. healthcare community.</p>
+
+    <p>As an alpha capability, the CDS Authoring Tool is targeted to a select audience for internal testing and validation. The alpha authoring tool has enough basic functionality to be used and to demonstrate the concept of authoring an artifact.  However, the alpha version is not fully polished and it lacks many of the features that would be required for the final production version. As we move to beta, the target audience will need to be expanded to a broader set of users for testing, validation, and acceptance.</p>
   </div>
 );
