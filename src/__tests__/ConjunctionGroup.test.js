f2ea50432bdd02ecfbe2f7244a26ea1edd061949
craigmcginley <craigmcginley@gmail.com>
2017-08-03 09:28:00 -0400

    Add tests for better coverage in ConjunctionGroup component

--- src/__tests__/ConjunctionGroup.test.js
+++ src/__tests__/ConjunctionGroup.test.js
@@ -1,13 +1,19 @@
+import _ from 'lodash';
 import ConjunctionGroup from '../components/builder/ConjunctionGroup';
+import TemplateInstance from '../components/builder/TemplateInstance';
 import StringParameter from '../components/builder/parameters/StringParameter';
-import { fullRenderComponent, createTemplateInstance } from '../helpers/test_helpers';
+import { fullRenderComponent, shallowRenderComponent, createTemplateInstance } from '../helpers/test_helpers';
 import { instanceTree, elementGroups } from '../helpers/test_fixtures';
 
 let rootConjunction;
 let childConjunction;
+let shallowConjunction;
+let deeperConjunction;
+let childConjunctionPath;
 
 const operations = elementGroups.find(g => g.name === 'Operations');
 const orTemplate = operations.entries.find(e => e.id === 'Or');
+const andTemplate = operations.entries.find(e => e.id === 'And');
 const orInstance = createTemplateInstance(orTemplate);
 
 instanceTree.path = '';
@@ -18,25 +24,37 @@ getAllInstances.mockReturnValue(instanceTree.childInstances);
 
 const addInstance = jest.fn();
 const editInstance = jest.fn();
+const deleteInstance = jest.fn();
+
+const treeName = 'MeetsInclusionCriteria';
+
+const props = {
+  root: true,
+  name: treeName,
+  instance: instanceTree,
+  createTemplateInstance: createTemplateInstance,
+  addInstance,
+  editInstance,
+  updateInstanceModifiers: jest.fn(),
+  booleanParameters: [],
+  deleteInstance,
+  saveInstance: jest.fn(),
+  getAllInstances,
+  showPresets: jest.fn(),
+  categories: elementGroups
+};
 
 beforeEach(() => {
-  rootConjunction = fullRenderComponent(ConjunctionGroup, {
-    root: true,
-    name: 'Inclusions',
-    instance: instanceTree,
-    createTemplateInstance: createTemplateInstance,
-    addInstance,
-    editInstance,
-    updateInstanceModifiers: jest.fn(),
-    booleanParameters: [],
-    deleteInstance: jest.fn(),
-    saveInstance: jest.fn(),
-    getAllInstances,
-    showPresets: jest.fn(),
-    categories: elementGroups
-  });
-
+  shallowConjunction = shallowRenderComponent(ConjunctionGroup, props);
+  rootConjunction = fullRenderComponent(ConjunctionGroup, props);
   childConjunction = rootConjunction.find(ConjunctionGroup).at(1); // The 'Or' instance pushed into tree above
+  childConjunctionPath = childConjunction.node.getPath();
+});
+
+afterEach(() => {
+  addInstance.mockClear();
+  editInstance.mockClear();
+  deleteInstance.mockClear();
 });
 
 test('has correct base class', () => {
@@ -48,7 +66,7 @@ test('root level returns empty path', () => {
 });
 
 test('child level returns correct path', () => {
-  expect(childConjunction.node.getPath()).toEqual('.childInstances.2');
+  expect(childConjunctionPath).toEqual('.childInstances.2');
 });
 
 test('applies correct nesting class', () => {
@@ -61,7 +79,7 @@ test('adds children at correct tree position', () => {
 
   let argument = addInstance.mock.calls[0];
   argument[1].uniqueId = orInstance.uniqueId; // Unique ID generation tested in TemplateInstance.test.js
-                                                    // Make them the same to easily compare whilst avoiding React warning
+                                              // Make them the same to easily compare whilst avoiding React warning
 
   expect(addInstance).lastCalledWith(rootConjunction.props().name, orInstance, '');
 
@@ -69,7 +87,13 @@ test('adds children at correct tree position', () => {
   argument = addInstance.mock.calls[1];
   argument[1].uniqueId = orInstance.uniqueId;
 
-  expect(addInstance).lastCalledWith(rootConjunction.props().name, orInstance, '.childInstances.2');
+  expect(addInstance).lastCalledWith(rootConjunction.props().name, orInstance, childConjunctionPath);
+});
+
+test('can delete group', () => {
+  childConjunction.find('.conjunction-group__button-bar button').first().simulate('click');
+
+  expect(deleteInstance).toHaveBeenCalledWith(treeName, childConjunctionPath, []);
 });
 
 test('edits own type', () => {
@@ -88,5 +112,68 @@ test('edits own name', () => {
 
   nameParamater.find('input').simulate('change', { target: { name: 'element_name', value: newName } });
 
-  expect(editInstance).lastCalledWith(rootConjunction.props().name, { element_name: newName }, '.childInstances.2', false);
+  expect(editInstance).lastCalledWith(rootConjunction.props().name, { element_name: newName }, childConjunctionPath, false);
+});
+
+test('can\'t indent or outdent root group', () => {
+  expect(shallowConjunction.find('.indent-outdent-container')).toHaveLength(0);
+});
+
+test('can indent a child group', () => {
+  childConjunction.find('button[aria-label="indent"]').simulate('click');
+
+  const instance = createTemplateInstance(andTemplate, [childConjunction.node.props.instance]);
+  const path = childConjunctionPath.split('.').slice(0,-2).join('.');
+  const index = Number(childConjunctionPath.split('.').pop());
+
+  delete instance['uniqueId'];
+  delete deleteInstance.mock.calls[0][2][0].instance['uniqueId'];
+
+  expect(deleteInstance).toHaveBeenCalledWith(treeName, childConjunctionPath, [{ instance, path, index }]);
+});
+
+test('can outdent a child group', () => {
+  childConjunction.find('button[aria-label="outdent"]').simulate('click');
+
+  expect(deleteInstance).toHaveBeenCalledWith(treeName, childConjunctionPath, []);
+});
+
+describe('for deeper nested conjunction groups', () => {
+  beforeEach(() => {
+    const ageInstance = createTemplateInstance(elementGroups[0].entries[0]);
+    const deeperOr = _.cloneDeep(orInstance);
+    deeperOr.childInstances = [ ageInstance ];
+    const deeperTree = _.cloneDeep(instanceTree);
+    deeperTree.childInstances = [ deeperOr ];
+    const deeperProps = _.cloneDeep(props);
+    deeperProps.instance = deeperTree;
+    deeperConjunction = fullRenderComponent(ConjunctionGroup, deeperProps);
+    childConjunction = deeperConjunction.find(ConjunctionGroup).at(1);
+  });
+
+  // TODO: All the following should really verify what they were called with as well
+
+  test('can indent a child group', () => {
+    childConjunction.find('button[aria-label="indent"]').at(0).simulate('click');
+
+    expect(deleteInstance).toHaveBeenCalled();
+  });
+
+  test('can outdent a child group', () => {
+    childConjunction.find('button[aria-label="outdent"]').at(0).simulate('click');
+
+    expect(deleteInstance).toHaveBeenCalled();
+  });
+
+  test('can indent a child TemplateInstance', () => {
+    deeperConjunction.find(TemplateInstance).first().find('button[aria-label="indent"]').first().simulate('click');
+
+    expect(deleteInstance).toHaveBeenCalled();
+  });
+
+  test('can outdent a child TemplateInstance', () => {
+    deeperConjunction.find(TemplateInstance).first().find('button[aria-label="outdent"]').first().simulate('click');
+
+    expect(deleteInstance).toHaveBeenCalled();
+  });
 });
