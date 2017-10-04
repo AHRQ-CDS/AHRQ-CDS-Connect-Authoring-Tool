97708b344cc75453c34e250dfedd77f88148961f
Julia Afeltra <jafeltra@mitre.org>
2017-10-04 10:32:48 -0400

    Artifact API get, post, put, delete only artifacts for authenticated user

--- app/handlers/artifactHandler.js
+++ app/handlers/artifactHandler.js
@@ -10,49 +10,71 @@ module.exports = {
 
 // Get all artifacts
 function allGet(req, res) {
-  // eslint-disable-next-line array-callback-return
-  Artifact.find((error, artifacts) => {
-    if (error) res.status(500).send(error);
-    else res.json(artifacts);
-  });
+  if (req.user) {
+    // eslint-disable-next-line array-callback-return
+    Artifact.find({ user: req.user.uid }, (error, artifacts) => {
+      if (error) res.status(500).send(error);
+      else res.json(artifacts);
+    });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 // Get a single artifact
 function singleGet(req, res) {
-  const id = req.params.artifact;
-  Artifact.find({ _id: id }, (error, artifact) => {
-    if (error) res.status(500).send(error);
-    else res.json(artifact);
-  });
+  if (req.user) {
+    const id = req.params.artifact;
+    Artifact.find({ user: req.user.uid, _id: id }, (error, artifact) => {
+      if (error) res.status(500).send(error);
+      else res.json(artifact);
+    });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 // Post a single artifact
 function singlePost(req, res) {
-  Artifact.create(req.body,
-    (error, response) => {
-      if (error) res.status(500).send(error);
-      else res.status(201).json(response);
-    });
+  if (req.user) {
+    const newArtifact = req.body;
+    newArtifact.user = req.user.uid;
+    Artifact.create(newArtifact,
+      (error, response) => {
+        if (error) res.status(500).send(error);
+        else res.status(201).json(response);
+      });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 // Update a single artifact
 function singlePut(req, res) {
-  const id = req.body._id;
-  const artifact = req.body;
-  Artifact.update(
-    { _id: id },
-    { $set: artifact },
-    (error, response) => {
-      if (error) res.status(500).send(error);
-      else res.sendStatus(200);
-    });
+  if (req.user) {
+    const id = req.body._id;
+    const artifact = req.body;
+    Artifact.update(
+      { user: req.user.uid, _id: id },
+      { $set: artifact },
+      (error, response) => {
+        if (error) res.status(500).send(error);
+        else res.sendStatus(200);
+      });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 // Delete a single artifact
 function singleDelete(req, res) {
-  const id = req.params.artifact;
-  Artifact.remove({ _id: id }, (error, response) => {
-    if (error) res.status(500).send(error);
-    else res.sendStatus(200);
-  });
+  if (req.user) {
+    const id = req.params.artifact;
+    Artifact.remove({ user: req.user.uid, _id: id }, (error, response) => {
+      if (error) res.status(500).send(error);
+      else res.sendStatus(200);
+    });
+  } else {
+    res.sendStatus(401);
+  }
 }
