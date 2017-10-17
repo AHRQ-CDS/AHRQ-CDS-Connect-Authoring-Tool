375eb78c0ebcb6661fa34d19dee3f08839f12601
Chris Moesel <cmoesel@mitre.org>
2017-10-17 15:56:19 -0400

    Style NotFound and NotLoggedIn pages

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default ({ location }) => (
-  <div>
-    <h3>No match for <code>{location.pathname}</code></h3>
+  <div className="notFound" id="maincontent">
+    <div className="notFound-wrapper">
+      <h3>No match for <code>{location.pathname}</code></h3>
+    </div>
   </div>
 );
