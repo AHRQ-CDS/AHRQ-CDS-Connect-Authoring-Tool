9aa8b86b7d64610f5cbd963f4ed444abf031d829
Julia Afeltra <jafeltra@mitre.org>
2017-10-18 13:32:59 -0400

    Added new nav bar options for logged in and logged out user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,25 +8,36 @@ export default class Navbar extends Component {
 
     if (isAuthenticated) {
       return (
-        <nav className="navbar__wrapper" aria-labelledby="cds-main-navigation">
-          <div className="sr-only" id="cds-main-navigation">Main navigation</div>
-
           <ul>
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/artifacts">Artifacts</NavLink></li>
             <li><NavLink to="/build">Workspace</NavLink></li>
+            {/* TODO: Add link. */}
+            <li><a href="">Documentation</a></li>
+            <li><a href="mailto:cds-authoring-list@lists.mitre.org">Feedback</a></li>
           </ul>
-        </nav>
       );
     }
 
-    return null;
+    return (
+        <ul>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          {/* TODO: Add link. */}
+          <li><a href="">Request an Account</a></li>
+          {/* TODO: Add link. */}
+          <li><a href="">Documentation</a></li>
+          <li><a href="mailto:cds-authoring-list@lists.mitre.org">Feedback</a></li>
+        </ul>
+    );
   }
 
   render() {
     return (
       <div className="navbar">
-        {this.renderedNavbar()}
+        <nav className="navbar__wrapper" aria-labelledby="cds-main-navigation">
+          <div className="sr-only" id="cds-main-navigation">Main navigation</div>
+          {this.renderedNavbar()}
+        </nav>
       </div>
     );
   }
