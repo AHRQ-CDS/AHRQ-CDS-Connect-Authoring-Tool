656dba70b00256220343964b203f500bb1e60a47
Chris Moesel <cmoesel@mitre.org>
2017-10-19 16:17:03 -0400

    Landing page text edits from AHRQ

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 export default () => (
   <div className="landing" id="maincontent">
     <div className="landing-wrapper">
-      <h2>Welcome to the Clinical Decision Support Authoring Tool</h2>
-
-      <p>
-        Build and export your first Clinicial Decision Support artifact by clicking
-        <NavLink to="/build"> here</NavLink>.
-      </p>
-
-      <h3>About</h3>
-
       <p>
         The <strong>CDS Authoring Tool</strong> is a key part of CDS Connect, a project sponsored by the &nbsp;
         <a href="https://www.ahrq.gov/">Agency for Healthcare Research and Quality</a> that will offer a systematic
         and replicable process for transforming patient-centered outcomes research (PCOR) findings into shareable and
-        standards-based clinical decision support (CDS) digital, computable artifacts.
+        standards-based clinical decision support (CDS) digital, computable artifacts. CDS Connect will function as a
+        repository of CDS artifacts and create prototype infrastructure for sharing CDS across different health care
+        settings and technologies.
       </p>
 
       <p>
-        The CDS Authoring Tool, along with the <a href="https://cds.ahrq.gov">CDS Connect Repository</a>, will
-        promote the creation and use of CDS in everyday clinical settings, and that it will serve as the linchpin for
+        The CDS Authoring Tool, along with the <a href="https://cds.ahrq.gov/cdsconnect">CDS Connect Repository</a>,
+        will promote the creation and use of CDS in everyday clinical settings, and will serve as the linchpin for
         connecting high-quality CDS to the U.S. healthcare community.
       </p>
 
