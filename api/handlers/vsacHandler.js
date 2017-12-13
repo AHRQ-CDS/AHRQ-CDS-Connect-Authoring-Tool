4cea0ac4fa5002b8b204365c1deeb8b11af6a78b
Julia Afeltra <jafeltra@mitre.org>
2017-12-13 14:26:51 -0500

    Only save ticket on session, let VSAC handle age of ticket

--- api/handlers/vsacHandler.js
+++ api/handlers/vsacHandler.js
@@ -5,10 +5,7 @@ function login(req, res) {
   const pass = req.body.password;
   vsacClient.getTicketGrantingTicket(user, pass)
     .then(t => {
-      req.session.ticketGrantingTicket = {
-        ticket: t,
-        timeCreated: new Date()
-      };
+      req.session.ticketGrantingTicket = t;
       res.sendStatus(200);
     })
     .catch(error => {
@@ -18,25 +15,18 @@ function login(req, res) {
 }
 
 function getVSDetailsByOID(req, res) {
-  const sessionTicketInfo = req.session.ticketGrantingTicket;
-  if (sessionTicketInfo) {
-    const ageOfTGT = new Date() - new Date(sessionTicketInfo.timeCreated);
-    if (ageOfTGT < 28800000) {
-      // TGT was created less than 8 hours ago and is valid. Generate ST to use to get VS details.
-      vsacClient.getServiceTicket(sessionTicketInfo.ticket)
-        .then(serviceTicket => {
-          return vsacClient.getVSDetailsByOID(req.params.id, serviceTicket);
-        })
-        .then(details => {
-          res.json(details);
-        })
-        .catch(error => {
-          res.sendStatus(error.statusCode);
-        });
-    } else {
-      // TGT created more than 8 hours ago and no longer valid.
-      res.sendStatus(401);
-    }
+  const ticketGrantingTicket = req.session.ticketGrantingTicket;
+  if (ticketGrantingTicket) {
+    vsacClient.getServiceTicket(ticketGrantingTicket)
+      .then(serviceTicket => {
+        return vsacClient.getVSDetailsByOID(req.params.id, serviceTicket);
+      })
+      .then(details => {
+        res.json(details);
+      })
+      .catch(error => {
+        res.sendStatus(error.statusCode);
+      });
   } else {
     // No TGT on the session. Must login first.
     res.sendStatus(401);
