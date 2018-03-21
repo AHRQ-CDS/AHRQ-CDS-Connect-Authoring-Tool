095bb652810ea16dab82feb6e1a5cf70ba8cfb1a
Andrew Schreiber <schreiaj@gmail.com>
2018-03-21 15:00:34 -0400

    added tests

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -1,15 +1,17 @@
+const exists = el => el !== undefined && el !== '' && el !== null
+
 export default {
   or: {
-    check: (values, args = []) => values.some(el => el),
+    check: (values, args = []) => values.some(exists),
     warning: (fields, args) => `You should provide at least one of the following: ${fields.join(', ')}.`
   },
   and: {
-    check: (values, args = []) => values.every(el => el),
+    check: (values, args = []) => values.every(exists),
     warning: (fields, args) => `You should provide all of the following: ${fields.join(', ')}.`
   },
   // Ok, this is more or less the exact same thing as 'and' but the warning is different
   require: {
-    check: (values, args = []) => values.every(el => el),
+    check: (values, args = []) => values.every(exists),
     warning: (fields, args) => `The following fields are required: ${fields.join(', ')}.`
   },
   regex: {
@@ -17,13 +19,13 @@ export default {
       const regex = RegExp(args[0]);
       return values.every(el => regex.test(el));
     },
-    warning: (fields, args) => `${fields.join(', ')} must conform to the pattern ${RegExp(args[0])}`
+    warning: (fields, args) => `${fields.join(', ')} must conform to the pattern ${RegExp(args[0])}.`
   },
   ifThen: {
     check: (values, args = []) => {
-      if (values[0]) { return values.every(el => el); }
+      if (exists(values[0])) { return values.every(exists); }
       return true;
     },
-    warning: (fields, args) => `You must specify ${fields.slice(1).join(', ')} if you specify ${fields[0]}`
+    warning: (fields, args) => `You must specify ${fields.slice(1).join(', ')} if you specify ${fields[0]}.`
   }
 };
