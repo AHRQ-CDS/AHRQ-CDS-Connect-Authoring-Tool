40242bda78c5e80795e19e0f2f137d988b32b7da
Noranda Brown <nebrown@mitre.org>
2018-07-25 15:03:47 -0400

    Updated patient view to list resources

--- frontend/src/components/patient/PatientDataSection.js
+++ frontend/src/components/patient/PatientDataSection.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Table, Collapse, Button } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
 
 export default class PatientDataSection extends Component {
-  renderHeader = title => {
+  constructor(props) {
+    super(props);
+
+    this.state = { collapse: false };
+  }
+
+  toggle = () => {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  renderHeader = (title) => {
+    const chevronIcon = this.state.collapse ? 'chevron-down' : 'chevron-right';
+
     return (
-      <div className="patient-view__header">
+      <div className="patient-data-section__header">
         <div className="header-title">{title}</div>
         <div className="header-divider"></div>
+        <Button onClick={this.toggle} className="header-button"><FontAwesome name={chevronIcon} /></Button>
       </div>
     );
   }
 
+  renderTable = (type, data) => {
+    const keys = Object.keys(data[0]);
+
+    return (
+      <Table className="patient-data-section__table">
+        <thead>
+          <tr>
+            {keys.map((key, i) => <th key={i}>{key}</th>)}
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((element, i) =>
+            <tr key={i}>
+              {keys.map((key, j) => <td key={j}>{element[key]}</td>)}
+            </tr>)
+          }
+        </tbody>
+      </Table>
+    );
+  }
+
   render() {
-    const { resource } = this.props;
+    const { title, data } = this.props;
 
     return (
       <div className="patient-data-section">
-        {resource}
+        {this.renderHeader(title)}
+
+        <Collapse isOpen={this.state.collapse}>
+          {data && this.renderTable(title, data)}
+        </Collapse>
       </div>
     );
   }
 }
 
 PatientDataSection.propTypes = {
-  resource: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  data: PropTypes.array
 };
--- frontend/src/components/patient/PatientView.js
+++ frontend/src/components/patient/PatientView.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Inspector from 'react-inspector';
-import { Table, Collapse } from 'reactstrap';
 
 import getProperty from '../../utils/getProperty';
 import patientProps from '../../prop-types/patient';
@@ -9,61 +7,51 @@ import patientProps from '../../prop-types/patient';
 import PatientDataSection from './PatientDataSection';
 
 const RESOURCE_KEYS = {
-  'Patient': {
-    'Family name': 'name.firstObject.family.firstObject',
-    'Given name': 'name.firstObject.given.firstObject'
-  },
-  'Organization': {
-    'Name': 'name',
-    'Type': 'type.coding.firstObject.display',
+  Organization: {
+    Name: 'name',
+    Type: 'type.coding.firstObject.display',
     'Street address': 'address.firstObject.line.firstObject',
-    'City': 'address.firstObject.city',
-    'State': 'address.firstObject.state',
+    City: 'address.firstObject.city',
+    State: 'address.firstObject.state',
     'Zip code': 'address.firstObject.postalCode',
-    'Phone': 'telecom.firstObject.value'
+    Phone: 'telecom.firstObject.value'
   },
-  'Encounter': {
-    'Type': 'type.firstObject.coding.text',
+  Encounter: {
+    Type: 'type.firstObject.text',
     'Start date': 'period.start',
     'End date': 'period.end',
-    'Status': 'status'
+    Status: 'status'
   },
-  'Observation': {
-    'Category': 'category.text',
-    'Type': 'code.text',
-    'Value': 'valueQuantity.value',
-    'Unit': 'valueQuantity.unit'
+  Observation: {
+    Category: 'category.text',
+    Type: 'code.text',
+    Value: 'valueQuantity.value',
+    Unit: 'valueQuantity.unit'
   },
-  'Immunization': {
-    'Vaccine': 'vaccineCode.text',
-    'Date': 'date',
-    'Status': 'completed'
+  Immunization: {
+    Vaccine: 'vaccineCode.text',
+    Date: 'date',
+    Status: 'status'
   },
-  'Claim': {
-    'Type': 'type',
+  Claim: {
+    Type: 'type',
     'First item': 'item.firstObject.type.display'
   },
-  'Procedure': {
-    'Procedure': 'code.text',
-    'Date': 'code.performedDateTime'
+  Procedure: {
+    Procedure: 'code.text',
+    Date: 'performedDateTime'
   }
 };
 
 export default class PatientView extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { collapse: false };
-  }
-
-  extractData = resourceType => {
+  extractData = (resourceType) => {
     const { patient } = this.props;
     const resources = patient.patient.entry.filter(entry => entry.resource.resourceType === resourceType);
 
-    let data = [];
+    const data = [];
     resources.forEach((resource) => {
-      let row = {};
-      Object.keys(RESOURCE_KEYS[resourceType]).forEach(key => {
+      const row = {};
+      Object.keys(RESOURCE_KEYS[resourceType]).forEach((key) => {
         row[key] = getProperty(resource.resource, RESOURCE_KEYS[resourceType][key]);
       });
       data.push(row);
@@ -72,66 +60,36 @@ export default class PatientView extends Component {
     return data;
   }
 
-  renderHeader = title => {
-    return (
-      <div className="patient-view__header">
-        <div className="header-title">{title}</div>
-        <div className="header-divider"></div>
-      </div>
-    );
-  }
-
-  renderData = (type, data) => {
-    const keys = Object.keys(RESOURCE_KEYS[type]);
-
-    return (
-      <Table className="section-data-element">
-        <thead>
-          <tr>
-            {keys.map((key, i) => <th key={i}>{key}</th>)}
-          </tr>
-        </thead>
-
-        <tbody>
-          {data.map((element, i) =>
-            <tr key={i}>
-              {keys.map((key, j) => <td key={j}>{element[key]}</td>)}
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    );
-  }
-
-  renderSection = type => {
-    const data = this.extractData(type);
-
-    return (
-      <div className="patient-view__section">
-        {this.renderHeader(type)}
-        {data &&
-          <div className="section-data">
-            {this.renderData(type, data)}
-          </div>
-        }
-      </div>
-    );
-  }
-
   render() {
     const { patient } = this.props;
+    const patientInfo = patient.patient.entry[0].resource;
+    const patientName = patientInfo.name[0];
 
     return (
       <div className="patient-view">
-        <PatientDataSection resource='Patient' />
+        <div className="patient-view__patient-data">
+          <div className="patient-data-row">
+            <div className="patient-data-label">Patient name:</div>
+            <div className="patient-data-text">{`${patientName.given[0]} ${patientName.family[0]}`}</div>
+          </div>
 
-        {this.renderSection('Patient')}
-        {this.renderSection('Organization')}
-        {this.renderSection('Encounter')}
-        {this.renderSection('Observation')}
-        {this.renderSection('Immunization')}
-        {this.renderSection('Claim')}
-        {this.renderSection('Procedure')}
+          <div className="patient-data-row">
+            <div className="patient-data-label">Patient gender:</div>
+            <div className="patient-data-text">{patientInfo.gender}</div>
+          </div>
+
+          <div className="patient-data-row">
+            <div className="patient-data-label">Patient DOB:</div>
+            <div className="patient-data-text">{patientInfo.birthDate}</div>
+          </div>
+        </div>
+
+        <PatientDataSection title='Organizations' data={this.extractData('Organization')} />
+        <PatientDataSection title='Encounters' data={this.extractData('Encounter')} />
+        <PatientDataSection title='Observations' data={this.extractData('Observation')} />
+        <PatientDataSection title='Immunizations' data={this.extractData('Immunization')} />
+        <PatientDataSection title='Claims' data={this.extractData('Claim')} />
+        <PatientDataSection title='Procedures' data={this.extractData('Procedure')} />
 
         <hr/>
         <Inspector data={this.props.patient}/>
