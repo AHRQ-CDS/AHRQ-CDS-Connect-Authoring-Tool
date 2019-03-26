a60872e99ca93ea9ad45976cc4f5bf7d061539c0
Noranda Brown <nebrown@mitre.org>
2019-03-26 15:09:00 -0400

    Added public url prefix to images

--- frontend/src/data/whatsNew.js
+++ frontend/src/data/whatsNew.js
@@ -5,15 +5,15 @@ export default [
   {
     id: 1,
     name: 'Comments',
-    image: '/assets/images/whats-new-comments.png',
+    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-comments.png`,
     description: 'CDS Authors now have the ability to add comments to individual CDS elements including single base elements. These comments are added to the generated CQL above the element’s definition.',
     linkText: 'Documentation',
-    link: '/userguide#Comments'
+    link: `${process.env.PUBLIC_URL}/userguide#Comments`
   },
   {
     id: 2,
     name: 'Webinar',
-    image: '/assets/images/whats-new-webinar.png',
+    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-webinar.png`,
     description: 'AHRQ hosted a national web conference on the CDS Authoring Tool in February 2019.  During this conference, participants were shown how to build an example Statin Use artifact and how to integrate it with an EHR using CQL Services and the CDS Hooks API.',
     linkText: 'More Information',
     link: 'https://healthit.ahrq.gov/events/national-web-conference-clinical-decision-support-authoring-tool'
