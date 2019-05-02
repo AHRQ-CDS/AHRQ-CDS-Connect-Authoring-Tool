9659fd3da5f698b44f3af533a023bf24d23d89d2
Julia Afeltra <jafeltra@mitre.org>
2019-05-02 17:37:00 -0400

    Check for duplicate name and version of library before saving to DB.

--- api/handlers/externalCQLHandler.js
+++ api/handlers/externalCQLHandler.js
@@ -76,13 +76,23 @@ function singlePost(req, res) {
         }
       });
 
-      CQLLibrary.create(elmResults,
-        (error, response) => {
-          if (error) {
-            res.status(500).send(error);
+      CQLLibrary.find({ user: req.user.uid, linkedArtifactId: elmResults.linkedArtifactId }, (error, libraries) => {
+        if (error) res.status(500).send(error);
+        else {
+          const dupLibrary = libraries.find(lib => lib.name === elmResults.name && lib.version === elmResults.version);
+          if (!dupLibrary) {
+            CQLLibrary.create(elmResults,
+              (error, response) => {
+                if (error) {
+                  res.status(500).send(error);
+                }
+                else res.status(201).json(response);
+              });
+          } else {
+            res.status(409).send('Library with identical name and version already exists.');
           }
-          else res.status(201).json(response);
-        });
+        }
+      });
     });
 
   } else {
