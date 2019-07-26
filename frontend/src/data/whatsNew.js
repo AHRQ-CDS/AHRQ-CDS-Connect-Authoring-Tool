9291db95036e9a8cc08096333b669cac710912a7
Chris Moesel <cmoesel@mitre.org>
2019-07-26 09:42:33 -0400

    Add External CQL to What's New

--- frontend/src/data/whatsNew.js
+++ frontend/src/data/whatsNew.js
@@ -4,6 +4,14 @@
 export default [
   {
     id: 1,
+    name: 'External CQL',
+    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-external-cql.png`,
+    description: 'CDS Authors can now import externally authored CQL libraries and use their defined expressions in Inclusions, Exclusions, Subpopulations, and Base Elements. This allows authors to fill gaps in logic that cannot be produced by the CDS Authoring Tool and to leverage pre-existing CQL expressions.',
+    linkText: 'Documentation',
+    link: `${process.env.PUBLIC_URL}/userguide#External_CQL`
+  },
+  {
+    id: 2,
     name: 'Comments',
     image: `${process.env.PUBLIC_URL}/assets/images/whats-new-comments.png`,
     description: 'CDS Authors now have the ability to add comments to individual CDS elements including single base elements. These comments are added to the generated CQL above the element’s definition.',
@@ -11,21 +19,13 @@ export default [
     link: `${process.env.PUBLIC_URL}/userguide#Comments`
   },
   {
-    id: 2,
+    id: 3,
     name: 'Webinar',
     image: `${process.env.PUBLIC_URL}/assets/images/whats-new-webinar.png`,
     description: 'AHRQ hosted a national web conference on the CDS Authoring Tool in February 2019.  During this conference, participants were shown how to build an example Statin Use artifact and how to integrate it with an EHR using CQL Services and the CDS Hooks API.',
     linkText: 'More Information',
     link: 'https://healthit.ahrq.gov/events/national-web-conference-clinical-decision-support-authoring-tool'
   }
-  // {
-  //   id: 3,
-  //   name: '',
-  //   image: '',
-  //   description: '',
-  //   linkText: '',
-  //   link: ''
-  // },
   // {
   //   id: 4,
   //   name: '',
