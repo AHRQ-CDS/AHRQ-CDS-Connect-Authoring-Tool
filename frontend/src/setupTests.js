6d3db63eb3398dccace51a4f4ed0935975999b5e
Noranda Brown <nebrown@mitre.org>
2019-09-05 10:02:22 -0400

    Initial upgrade include react and related packages, also upgraded react-select and reworked for new api -- todo: fix tests

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,4 +1,9 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ReactModal from 'react-modal';
+
+// configure enzyme
+Enzyme.configure({ adapter: new Adapter() });
 
 // disable modal root in tests
 ReactModal.setAppElement(null);
