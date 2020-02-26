613131b5c645cfde130b302c95be18b6ef9151fb
Chris Moesel <cmoesel@mitre.org>
2020-02-26 14:25:40 -0500

    CodeSelectModal: Replace ICD-10 w/ ICD-10-CM, Rename ICD-9 to ICD-9-CM
    The code select modal shouldn't allow users to choose base ICD-10, as VSAC doesn't support it.  So switch it to ICD-10-CM.
    
    ICD-9 already pointed to the ICD9CM URL, but to make it more clear, rename it to ICD-9-CM.
    
    This does not affect existing artifacts.  If they used ICD-10, they will retain the base ICD-10 unless/until an author removes the ICD-10 code and adds an ICD-10-CM code.

--- frontend/src/components/builder/CodeSelectModal.js
+++ frontend/src/components/builder/CodeSelectModal.js
@@ -208,8 +208,8 @@ export default class CodeSelectModal extends Component {
 
     const codeSystemOptions = [
       { value: 'SNOMED', label: 'SNOMED', id: 'http://snomed.info/sct' },
-      { value: 'ICD-9', label: 'ICD-9', id: 'http://hl7.org/fhir/sid/icd-9-cm' },
-      { value: 'ICD-10', label: 'ICD-10', id: 'http://hl7.org/fhir/sid/icd-10' },
+      { value: 'ICD-9-CM', label: 'ICD-9-CM', id: 'http://hl7.org/fhir/sid/icd-9-cm' },
+      { value: 'ICD-10-CM', label: 'ICD-10-CM', id: 'http://hl7.org/fhir/sid/icd-10-cm' },
       { value: 'NCI', label: 'NCI', id: 'http://ncimeta.nci.nih.gov' },
       { value: 'LOINC', label: 'LOINC', id: 'http://loinc.org' },
       { value: 'RXNORM', label: 'RXNORM', id: 'http://www.nlm.nih.gov/research/umls/rxnorm' },
