721f1226c86b69582641eda8d73689560706872f
Noranda Brown <nebrown@mitre.org>
2020-03-18 16:54:27 -0400

    Fixed ordering issues without using refs

--- frontend/src/components/builder/Recommendations.js
+++ frontend/src/components/builder/Recommendations.js
@@ -4,18 +4,16 @@ import update from 'immutability-helper';
 
 import Recommendation from './Recommendation';
 
+const UP = -1;
+const DOWN = 1;
+
 export default class Recommendations extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      mode: 'every',
-      //refsCollection: {},
-      moved: '',
+      mode: 'every'
     };
-    //this.refsCollection = {};
-    this.refList = {};
-
   }
 
   componentDidMount() {
@@ -24,17 +22,26 @@ export default class Recommendations extends Component {
     }
   }
 
-  componentDidUpdate() {
-    if (this.props.artifact.recommendations.length > 0) {
-      console.log("COMPONENT DID UPDATE!1!");
-      console.log(this.state.moved);
-      //this.refList[this.state.moved].focus(); //this blows up, as focus isn't a method of a ref
-      document.getElementById(this.state.moved).focus(); //this gets the element. yay!  but it doesn't actually work
-    }
-  }
-
   handleModeChange = (event) => {
     this.setState({ mode: event.target.value });
+  }
+
+  handleMove = (uid, direction) => {
+    const { recommendations } = this.props.artifact;
+    const position = recommendations.findIndex(index => index.uid === uid);
+    if (position < 0) {
+      throw new Error("Given recommendation not found.");
+    } else if (
+      (direction === UP && position === 0)
+      || (direction === DOWN && position === recommendations.length - 1)) {
+      return; // canot move outside of array
+    }
+
+    const recommendation = recommendations[position]; // save recommendation for later
+    const newRecommendations = recommendations.filter(rec => rec.uid !== uid); // remove recommendation from array
+    newRecommendations.splice(position + direction, 0, recommendation);
+
+    this.props.updateRecommendations(newRecommendations);
   }
 
   addRecommendation = () => {
@@ -66,43 +73,6 @@ export default class Recommendations extends Component {
     this.props.updateRecommendations(newRecs);
   }
 
-  moveRecommendationUp = (uid) => {
-    const index = this.props.artifact.recommendations.findIndex(rec=> rec.uid === uid);
-    if(index > 0){
-      const prev = index - 1;
-      const first = this.props.artifact.recommendations[prev];
-      const second = this.props.artifact.recommendations[index];
-      //const newRecs = this.props.artifact.recommendations.slice(0,index);
-      const newRecs = update(this.props.artifact.recommendations, {
-        $splice: [[prev, 2,second,first]]
-      });
-      this.props.updateRecommendations(newRecs);
-      var n = document.getElementById(uid);
-      console.log(n.id);
-    }
-    //console.log("move up event fired: caught in Recommendations");
-  }
-
-  moveRecommendationDown = (uid) => {
-    const index = this.props.artifact.recommendations.findIndex(rec=> rec.uid === uid);
-    if(index < this.props.artifact.recommendations.length-1) {
-      const next = index + 1;
-      const first = this.props.artifact.recommendations[index];
-      const second = this.props.artifact.recommendations[next];
-      const newRecs = update(this.props.artifact.recommendations, {
-        $splice: [[index, 2,second,first]]
-      });
-      this.setState({'moved':uid});
-      this.props.updateRecommendations(newRecs);
-      //const n = document.getElementById(uid);
-      //const n2 = this.refList[uid].ref;
-      //n.focus();
-      //n2.focus();
-      //console.log(n.id);
-      //console.log(n2)
-    }
-    //console.log("move down even fired: Caught in Recommendations");
-  }
   render() {
     return (
       <div className="recommendations">
@@ -134,7 +104,7 @@ export default class Recommendations extends Component {
         }
 
         {this.props.artifact.recommendations && this.props.artifact.recommendations.map((rec,i) => (
-          <div id={rec.uid}>
+          <div id={rec.uid} key={rec.uid}>
             <Recommendation
               key={rec.uid}
               artifact={this.props.artifact}
@@ -142,12 +112,11 @@ export default class Recommendations extends Component {
               rec={rec}
               onUpdate={this.updateRecommendation}
               onRemove={this.removeRecommendation}
-              onMoveRecUp={this.moveRecommendationUp}
-              onMoveRecDown={this.moveRecommendationDown}
+              onMoveRecUp={() => this.handleMove(rec.uid, UP)}
+              onMoveRecDown={() => this.handleMove(rec.uid, DOWN)}
               updateRecommendations={this.props.updateRecommendations}
               updateSubpopulations={this.props.updateSubpopulations}
               setActiveTab={this.props.setActiveTab}
-              ref={(el) => {this.refList[rec.uid] = el}}
             />
           </div>
         ))}
