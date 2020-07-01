f6f7341334d662124fecf16f3a74f6260c51b128
Chris Moesel <cmoesel@mitre.org>
2020-07-01 17:27:57 -0400

    Update What's New
    Add Common Functions and Collapsible Comments to What's New.

--- frontend/src/data/whatsNew.js
+++ frontend/src/data/whatsNew.js
@@ -4,6 +4,20 @@
 export default [
   {
     id: 1,
+    name: 'Common Functions',
+    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-common-functions.png`,
+    description: 'Artifact downloads no longer package separate CDS Connect Commons and CDS Connect Conversions libraries. Instead, the primary CQL library will embed any required common functions in its own CQL.',
+  },
+  {
+    id: 2,
+    name: 'Collapsible Comments',
+    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-collapsible-comments.png`,
+    description: 'User-provided comments are now supported on most authoring elements.  Comments are collapsed by default to allow users to see the most relevant data without being overwhelmed by empty comment blocks.',
+    linkText: 'Documentation',
+    link: `${process.env.PUBLIC_URL}/documentation#Provide_Additional_Information`
+  },
+  {
+    id: 3,
     name: 'FHIR R4 Support',
     image: `${process.env.PUBLIC_URL}/assets/images/whats-new-fhir-r4.png`,
     description: 'CDS Authors can now download and test their CDS artifacts using HL7 FHIR R4, the first normative edition of the FHIR specification. Support for FHIR R4 supplements existing support for FHIR DSTU2 and STU3.',
@@ -12,27 +26,11 @@ export default [
     linkExternal: true
   },
   {
-    id: 2,
+    id: 4,
     name: 'Multi-Patient Testing',
     image: `${process.env.PUBLIC_URL}/assets/images/whats-new-testing.png`,
     description: 'CDS Authors can now test their CDS artifacts against more than one synthetic patient at once. The selected CDS artifact will be executed against each patient the author selects, and individual results are displayed.',
     linkText: 'Documentation',
     link: `${process.env.PUBLIC_URL}/documentation#Testing_Artifacts`
-  },
-  {
-    id: 3,
-    name: 'External CQL',
-    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-external-cql.png`,
-    description: 'CDS Authors can now import externally authored CQL libraries and use their defined expressions in Inclusions, Exclusions, Subpopulations, and Base Elements. This allows authors to fill gaps in logic that cannot be produced by the CDS Authoring Tool and to leverage pre-existing CQL expressions.',
-    linkText: 'Documentation',
-    link: `${process.env.PUBLIC_URL}/documentation#External_CQL`
-  },
-  {
-    id: 4,
-    name: 'Comments',
-    image: `${process.env.PUBLIC_URL}/assets/images/whats-new-comments.png`,
-    description: 'CDS Authors now have the ability to add comments to individual CDS elements including single base elements. These comments are added to the generated CQL above the element’s definition.',
-    linkText: 'Documentation',
-    link: `${process.env.PUBLIC_URL}/documentation#Provide_Additional_Information`
   }
 ];
