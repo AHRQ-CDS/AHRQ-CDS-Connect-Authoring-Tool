cdba78c2930ecd7d6cc1e821ef8da31393686ec3
Noranda Brown <noranda@norandabrown.com>
2020-10-01 18:26:22 -0400

    Fixed modifiers action tests

--- frontend/src/actions/__tests__/modifiers.test.js
+++ frontend/src/actions/__tests__/modifiers.test.js
@@ -1,24 +1,49 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import moxios from 'moxios';
+import _ from 'lodash';
 
 import * as actions from '../modifiers';
 import * as types from '../types';
 
+import localModifiers from '../../data/modifiers';
+
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+const modifierMap = _.keyBy(localModifiers, 'id');
+const modifiersByInputType = {};
+
+localModifiers.forEach((modifier) => {
+  modifier.inputTypes.forEach((inputType) => {
+    modifiersByInputType[inputType] = (
+      modifiersByInputType[inputType] || []
+    ).concat(modifier);
+  });
+});
 
 describe('modifiers actions', () => {
   describe('loadModifiers', () => {
-    beforeEach(() => { moxios.install(); });
-    afterEach(() => { moxios.uninstall(); });
+    beforeEach(() => {
+      moxios.install();
+    });
+    afterEach(() => {
+      moxios.uninstall();
+    });
 
     it('dispatches a LOAD_MODIFIERS_SUCCESS action on successful load', () => {
-      const store = mockStore({});
+      const store = mockStore({
+        externalCQL: {
+          externalCqlList: []
+        }
+      });
 
       const expectedActions = [
         { type: types.MODIFIERS_REQUEST },
-        { type: types.LOAD_MODIFIERS_SUCCESS, modifierMap: {}, modifiersByInputType: {} }
+        {
+          type: types.LOAD_MODIFIERS_SUCCESS,
+          modifierMap,
+          modifiersByInputType
+        }
       ];
 
       return store.dispatch(actions.loadModifiers()).then(() => {
@@ -40,8 +65,12 @@ describe('modifiers actions', () => {
     });
   });
   describe('loadConversionFunctions', () => {
-    beforeEach(() => { moxios.install(); });
-    afterEach(() => { moxios.uninstall(); });
+    beforeEach(() => {
+      moxios.install();
+    });
+    afterEach(() => {
+      moxios.uninstall();
+    });
 
     it('dispatches a LOAD_CONVERSION_FUNCTIONS_SUCCESS action on successful load', () => {
       const store = mockStore({});
@@ -54,7 +83,10 @@ describe('modifiers actions', () => {
 
       const expectedActions = [
         { type: types.CONVERSION_FUNCTIONS_REQUEST },
-        { type: types.LOAD_CONVERSION_FUNCTIONS_SUCCESS, conversionFunctions: [] }
+        {
+          type: types.LOAD_CONVERSION_FUNCTIONS_SUCCESS,
+          conversionFunctions: []
+        }
       ];
 
       return store.dispatch(actions.loadConversionFunctions()).then(() => {
@@ -73,7 +105,11 @@ describe('modifiers actions', () => {
 
       const expectedActions = [
         { type: types.CONVERSION_FUNCTIONS_REQUEST },
-        { type: types.LOAD_CONVERSION_FUNCTIONS_FAILURE, status: 401, statusText: 'Unauthorized' }
+        {
+          type: types.LOAD_CONVERSION_FUNCTIONS_FAILURE,
+          status: 401,
+          statusText: 'Unauthorized'
+        }
       ];
 
       return store.dispatch(actions.loadConversionFunctions()).then(() => {
