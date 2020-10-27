e7ab885a6b2c2c51185e242906d8545472d4336e
Noranda Brown <noranda@norandabrown.com>
2020-10-27 10:50:07 -0400

    Upgraded change-case from 3.0.1 to 4.1.1

--- frontend/src/utils/strings.js
+++ frontend/src/utils/strings.js
@@ -1,24 +1,19 @@
 /* eslint-disable no-multi-spaces */
-import changeCase from 'change-case';
+import * as changeCase from 'change-case';
 
 export default function changeToCase(string, desiredCase) {
   const acceptedCases = [
     'camelCase',          // test string => testString
+    'capitalCase',        // test string => Test String
     'constantCase',       // test string => TEST_STRING
     'dotCase',            // test string => test.string
     'headerCase',         // test string => Test-String
-    'lowerCase',          // TEST STRING => test string
-    'lowerCaseFirst',     // TEST => tEST
     'noCase',             // test string => test string
     'paramCase',          // test string => test-string
     'pascalCase',         // test string => TestString
     'pathCase',           // test string => test/string
     'sentenceCase',       // testString => Test string
-    'snakeCase',          // test string => test_string
-    'swapCase',           // Test String => tEST sTRING
-    'titleCase',          // a simple test => A Simple Test
-    'upperCase',          // test string => TEST STRING
-    'upperCaseFirst'      // test => Test
+    'snakeCase'           // test string => test_string
   ];
 
   if (acceptedCases.indexOf(desiredCase) > -1) {
