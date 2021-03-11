6e710021b3e9ead49823218eab615aee0a90c256
Jacob Thomas <jsthomas@mitre.org>
2021-03-11 17:03:43 -0500

    Formatting via prettier

--- frontend/src/components/artifact/ArtifactTable.js
+++ frontend/src/components/artifact/ArtifactTable.js
@@ -1,77 +1,71 @@
 import { React, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel} from '@material-ui/core';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from '@material-ui/core';
 
 import ArtifactTableRow from './ArtifactTableRow';
 import { sortByName, sortByVersion, sortByDateEdited, sortByDateCreated } from 'utils/artifactSort';
 import artifactProps from 'prop-types/artifact';
 
-
 const ArtifactTable = ({ artifacts, handleDeleteArtifact, handleUpdateArtifact }) => {
-
-  const [ selectedColumn, setSelectedColumn ] = useState(0);
-  const [ columnOrder, setColumnOrder ] = useState("asc");
+  const [selectedColumn, setSelectedColumn] = useState(0);
+  const [columnOrder, setColumnOrder] = useState('asc');
 
   const createColumn = (columnName, columnSortHandler) => {
-    return { columnName : columnName, columnSortHandler : columnSortHandler};
+    return { columnName: columnName, columnSortHandler: columnSortHandler };
   };
 
   const columns = [];
-  columns.push(createColumn("Artifact Name", sortByName));
-  columns.push(createColumn("Version", sortByVersion));
-  columns.push(createColumn("Last Changed", sortByDateEdited));
-  columns.push(createColumn("Date Created", sortByDateCreated));
+  columns.push(createColumn('Artifact Name', sortByName));
+  columns.push(createColumn('Version', sortByVersion));
+  columns.push(createColumn('Last Changed', sortByDateEdited));
+  columns.push(createColumn('Date Created', sortByDateCreated));
 
-  const handleLabelClick = ( id ) => {
-    console.log("Selected Column:", id);
+  const handleLabelClick = id => {
+    console.log('Selected Column:', id);
 
     if (parseInt(id) === selectedColumn) {
-      setColumnOrder(columnOrder === "asc" ? "desc" : "asc");
+      setColumnOrder(columnOrder === 'asc' ? 'desc' : 'asc');
     } else {
-        setSelectedColumn(parseInt(id));
-        setColumnOrder("asc");
+      setSelectedColumn(parseInt(id));
+      setColumnOrder('asc');
     }
   };
 
-  const isActiveColumn = ( columnIndex ) => (selectedColumn === columnIndex ? true : false);
+  const isActiveColumn = columnIndex => (selectedColumn === columnIndex ? true : false);
 
   const getActiveSort = () => {
     const activeSort = columns[selectedColumn].columnSortHandler;
     return (a, b) => {
-        return (columnOrder === "asc" ? 1 : -1) * activeSort(a,b);
+      return (columnOrder === 'asc' ? 1 : -1) * activeSort(a, b);
     };
   };
-
-
 
   return (
     <TableContainer>
       <Table aria-label="artifact table">
         <TableHead>
           <TableRow>
-
-            {
-              columns.map((column, index) => {
-                return (
-                  <TableCell key={index} padding="none">
-                  <TableSortLabel 
+            {columns.map((column, index) => {
+              return (
+                <TableCell key={index} padding="none">
+                  <TableSortLabel
                     id={index}
-                    direction={isActiveColumn(index) ? columnOrder : "asc"}
+                    direction={isActiveColumn(index) ? columnOrder : 'asc'}
                     active={isActiveColumn(index)}
-                    onClick={() => handleLabelClick(index)}>
-                      {column.columnName}
+                    onClick={() => handleLabelClick(index)}
+                  >
+                    {column.columnName}
                   </TableSortLabel>
                 </TableCell>
-                );
-              })
-            }
+              );
+            })}
 
             <TableCell></TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {artifacts.map((art) => console.log(art))}
+          {artifacts.map(art => console.log(art))}
           {artifacts.sort(getActiveSort()).map(artifact => (
             <TableRow key={artifact._id}>
               <ArtifactTableRow
@@ -85,7 +79,6 @@ const ArtifactTable = ({ artifacts, handleDeleteArtifact, handleUpdateArtifact }
       </Table>
     </TableContainer>
   );
-
 };
 
 ArtifactTable.propTypes = {
--- frontend/src/utils/artifactSort.js
+++ frontend/src/utils/artifactSort.js
@@ -1,37 +1,32 @@
 import { sortMostRecent } from './sort';
 
 export function sortByDateEdited(a, b) {
-    return sortMostRecent(a, b);
+  return sortMostRecent(a, b);
+}
+
+export function sortByDateCreated(a, b) {
+  if (a.createdAt > b.createdAt || (a.createdAt && !b.createdAt)) {
+    return -1;
+  } else if (a.createdAt < b.createdAt || (!a.createdAt && b.createdAt)) {
+    return 1;
   }
-  
-  export function sortByDateCreated(a, b) {
-    if (a.createdAt > b.createdAt || (a.createdAt && !b.createdAt)) {
-      return -1;
-    } else if (a.createdAt < b.createdAt || (!a.createdAt && b.createdAt)) {
-      return 1;
-    }
-    return 0;
+  return 0;
+}
+
+export function sortByVersion(a, b) {
+  const digits_a = a.version.split('.').map(strNumeral => parseInt(strNumeral));
+  const digits_b = b.version.split('.').map(strNumeral => parseInt(strNumeral));
+  if (digits_a.length !== 3 && digits_b.length === 3) return 1;
+  else if (digits_a.length === 3 && digits_b.length !== 3) return -1;
+
+  for (let digitIndex = 0; digitIndex < 3; digitIndex++) {
+    if (digits_a[digitIndex] > digits_b[digitIndex]) return -1;
+    else if (digits_a[digitIndex] === digits_b[digitIndex]) continue;
+    else return 1;
   }
-  
-  export function sortByVersion(a, b) {
-    const digits_a = a.version.split(".").map((strNumeral) => parseInt(strNumeral));
-    const digits_b = b.version.split(".").map((strNumeral) => parseInt(strNumeral));
-    if (digits_a.length !== 3 && digits_b.length === 3)
-      return 1;
-    else if (digits_a.length === 3 && digits_b.length !== 3)
-      return -1;
-  
-    for (let digitIndex = 0; digitIndex < 3; digitIndex++) {
-      if (digits_a[digitIndex] > digits_b[digitIndex])
-        return -1;
-      else if (digits_a[digitIndex] === digits_b[digitIndex])
-        continue;
-      else
-        return 1;
-    }
-    return 0;
-  }
-  
-  export function sortByName(a, b) {
-    return a.name.localeCompare(b.name);
-  }
+  return 0;
+}
+
+export function sortByName(a, b) {
+  return a.name.localeCompare(b.name);
+}
