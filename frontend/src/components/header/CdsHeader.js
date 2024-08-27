c21adf4dcfb2713a4a794f313ad64d8cde7b5d40
Julia Afeltra <jafeltra@mitre.org>
2024-08-27 13:30:34 +0000

    Merged in update-banner-language (pull request #649)
    Update language to include links
    
    * Update language to include links
    
    
    Approved-by: Chris Moesel

--- frontend/src/components/header/CdsHeader.js
+++ frontend/src/components/header/CdsHeader.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 import { Login, Logout } from 'components/auth';
+import { Link } from 'components/elements';
 import darkTheme from 'styles/theme';
 
 const CdsHeader = () => {
@@ -15,11 +16,17 @@ const CdsHeader = () => {
       <ThemeProvider theme={darkTheme}>
         <div className="cds-header-alert">
           Planned for August 29, 2024, the CDS Connect Project will be transitioning into a temporary hiatus. This
-          transition is part of establishing a new sustainment model for CDS Connect related to the recent Challenge
-          Competition and Request for Information. The creation of new user accounts will continue routinely until
-          09/12/2024. After that time, account creation may be delayed. Platform services are expected to remain
-          available during this time but may experience occasional outages during this hiatus. For updates on the hiatus
-          or to report an issue, please contact the AHRQ team at clinicaldecisionsupport@ahrq.hhs.gov.
+          transition is part of establishing a new sustainment model for CDS Connect related to the recent{' '}
+          <Link text={'challenge competition'} href="https://www.ahrq.gov/challenges/cds-connect/index.html" /> and{' '}
+          <Link
+            text={'Request for Information'}
+            href="https://www.federalregister.gov/documents/2024/06/03/2024-11878/cds-connect-designing-the-future-of-a-national-hub-for-clinical-decision-support-request-for"
+          />
+          . The creation of new user accounts will continue routinely until 09/12/2024. After that time, account
+          creation may be delayed. Platform services are expected to remain available during this time but may
+          experience occasional outages during this hiatus. For updates on the hiatus or to report an issue, please
+          contact the AHRQ team at{' '}
+          <Link text={'clinicaldecisionsupport@ahrq.hhs.gov'} href="mailto:clinicaldecisionsupport@ahrq.hhs.gov" />.
         </div>
         <header className="cds-header">
           <div className="cds-header__cdsbanner">
